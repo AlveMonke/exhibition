7412eca2a242448aa20e00494282ca65e66dc11f
AlveMonke <89086047+AlveMonke@users.noreply.github.com>
2022-01-10 16:22:01 +0530

    Add files via upload

---
+++ intro_page/app.js
@@ -0,0 +1,155 @@
+inputs = document.querySelectorAll("input");
+
+let lat = 22.619916909941367;
+let lng = 88.35055541044872;
+
+let fromLocation, toLocation;
+
+let allowedPermission = false;
+
+navigator.geolocation.getCurrentPosition((e) => {
+    lat = e.coords.latitude;
+    lng = e.coords.longitude;
+    allowedPermission = true;
+});
+
+inputs.forEach((element) => {
+    element.addEventListener("input", async (event) => {
+        if (element.value == "") {
+            console.log("Wwha");
+
+            let x = document.getElementsByClassName("autocomplete-items");
+
+            Array.from(x).forEach((e) => {
+                e.remove();
+            });
+        } else {
+            let data = await fetch(
+                `https://places.ls.hereapi.com/places/v1/autosuggest?apiKey=ykWuUVoGdx-KpKO4Yj2mIpl-DeLo34_rhPSXQC0Cm_g&at=${lat},${lng}&q=${element.value}&pretty&result_types=address,place`
+            );
+            data = await data.json();
+
+            console.log(typeof data);
+            console.log(data);
+
+            if (element.value != "")
+                createAutoComplete(event.target, data.results);
+        }
+    });
+});
+
+function createAutoComplete(ele, arr) {
+    let a = document.createElement("div");
+    a.setAttribute("id", ele.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+
+    ele.parentNode.appendChild(a);
+
+    if (allowedPermission) {
+        let myLocation = document.createElement("DIV");
+        myLocation.innerHTML = "Your current Location";
+        myLocation.style.fontWeight = "200";
+        myLocation.innerHTML += `<input type='hidden' value='${lat}, ${lng}'>`;
+
+        myLocation.addEventListener("click", function (e) {
+            ele.value = this.getElementsByTagName("input")[0].value;
+            closeAllLists();
+        });
+        a.appendChild(myLocation);
+    }
+
+    if (arr.length === 0) {
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            x[i].parentElement.removeChild(x[i]);
+        }
+        return;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!arr[i].position) continue;
+
+        let inputVal = arr[i].title;
+        inputVal += arr[i].vicinity ? ", " + arr[i].vicinity : "";
+
+        let b = document.createElement("DIV");
+        b.innerHTML = arr[i].highlightedTitle + ", " + arr[i].vicinity;
+        b.innerHTML += `<input type='hidden' value='${inputVal}'>`;
+        b.addEventListener("click", function (e) {
+            ele.value = this.getElementsByTagName("input")[0].value.replaceAll(
+                "<br/>",
+                ", "
+            );
+            closeAllLists();
+
+            if (ele.id == "startPlace") fromLocation = arr[i].position;
+            else if (ele.id == "endPlace") toLocation = arr[i].position;
+        });
+        a.appendChild(b);
+    }
+
+    function closeAllLists(elmnt) {
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != ele) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+}
+
+let radios = document.querySelectorAll(".radio");
+
+radios.forEach((element) => {
+    element.onclick = () => {
+        Array.from(element.parentElement.children).forEach((child) => {
+            child.classList.remove("selected");
+        });
+        element.classList.add("selected");
+    };
+});
+
+function clearAll() {
+    console.log("hey");
+    document.querySelector(".selected").classList.remove("selected");
+    inputs.forEach((e) => {
+        e.value = "";
+        e.innerText = "";
+    });
+
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+        x[i].parentElement.removeChild(x[i]);
+    }
+}
+
+function doneMove() {
+    let modeSelected = document.querySelector(".selected");
+
+    if (
+        fromLocation &&
+        toLocation &&
+        fromLocation.length > 0 &&
+        toLocation.length > 0 &&
+        modeSelected != null &&
+        modeSelected != undefined
+    ) {
+        document.body.style.transition = ".25s";
+        document.body.style.opacity = "0";
+        localStorage.setItem("FromLocation", `${fromLocation}`);
+        localStorage.setItem("ToLocation", `${toLocation}`);
+        localStorage.setItem("Mode", modeSelected.id);
+
+        console.log(localStorage);
+
+        setTimeout(() => {
+            let link = document.createElement("a");
+            link.href = "../map_page/index.html";
+            link.click();
+        }, 500);
+    }
+}
---
+++ map_page/main.js
@@ -0,0 +1,156 @@
+let platform = new H.service.Platform({
+    apikey: "ykWuUVoGdx-KpKO4Yj2mIpl-DeLo34_rhPSXQC0Cm_g",
+});
+
+let routeResult;
+
+let defaultLayers = platform.createDefaultLayers();
+
+let userLatLong = [52.51477270923461, 13.39846691425174];
+
+if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition((e) => {
+        userLatLong = [e.coords.latitude, e.coords.longitude];
+    });
+else
+    alert(
+        "Your browser does not support GeoLocation. Try using a different one."
+    );
+
+let map = new H.Map(
+    document.getElementById("mapContainer"),
+    defaultLayers.vector.normal.map,
+    {
+        center: {
+            lat: userLatLong[0],
+            lng: userLatLong[1],
+        },
+        zoom: 13,
+        pixelRatio: window.devicePixelRatio || 1,
+    }
+);
+
+let group = new H.map.Group();
+
+let ui = H.ui.UI.createDefault(map, defaultLayers);
+ui.getControl("zoom").setAlignment("top-right");
+
+ui.removeControl("mapsettings");
+ui.removeControl("scalebar");
+
+ui.addControl(
+    "custom_settings_control",
+    new H.ui.MapSettingsControl({
+        baseLayers: [
+            {
+                label: "Normal",
+                layer: defaultLayers.vector.normal.map,
+            },
+            {
+                label: "Satellite",
+                layer: defaultLayers.raster.satellite.map,
+            },
+            {
+                label: "Terrain",
+                layer: defaultLayers.raster.terrain.map,
+            },
+        ],
+    })
+);
+
+ui.getControl("custom_settings_control").setAlignment("top-left");
+
+document.querySelector(".H_imprint").remove();
+
+function onResult(result) {
+    console.log(result);
+
+    if (result.routes.length) {
+        routeResult = result;
+
+        result.routes[0].sections.forEach((section) => {
+            let linestring = H.geo.LineString.fromFlexiblePolyline(
+                section.polyline
+            );
+
+            let poly = H.geo.LineString.fromFlexiblePolyline(
+                section.polyline
+            ).getLatLngAltArray();
+
+            let actions = section.actions;
+            let turnLatLng = [];
+            // Add a marker for each maneuver
+            for (i = 0; i < actions.length; i += 1) {
+                let action = actions[i];
+                if (action.action != "turn") continue;
+
+                turnLatLng.push([
+                    poly[action.offset * 3],
+                    poly[action.offset * 3 + 1],
+                    action.direction,
+                ]);
+            }
+
+            localStorage.setItem("turns", JSON.stringify(turnLatLng));
+
+            console.log(turnLatLng);
+
+            let startMarker = new H.map.Marker(
+                section.departure.place.location
+            );
+
+            let endMarker = new H.map.Marker(section.arrival.place.location);
+
+            let routeLine = new H.map.Polyline(linestring, {
+                style: { strokeColor: "#69bc8a", lineWidth: 3 },
+            });
+
+            group.addObjects([routeLine, startMarker, endMarker]);
+
+            map.addObjects([group]);
+
+            map.getViewModel().setLookAtData(
+                {
+                    bounds: group.getBoundingBox(),
+                },
+                true
+            );
+        });
+    }
+}
+
+let router = platform.getRoutingService(null, 8);
+let markCoord = [null, null];
+
+window.addEventListener("resize", () => map.getViewPort().resize());
+
+new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+let modeTransport = localStorage.getItem("Mode");
+if (modeTransport == "cycle") modeTransport = "bicycle";
+else if (modeTransport == "walk") modeTransport = "pedestrian";
+
+let routingParameters = {
+    routingMode: "fast",
+    transportMode: modeTransport,
+    origin: localStorage.getItem("FromLocation"),
+    destination: localStorage.getItem("ToLocation"),
+    return: "polyline,turnByTurnActions,actions,instructions,travelSummary",
+    waypoint:
+        "[geo!][Type[,StopOverDuration]!]Position[;TransitRadius[;UserLabel[;Heading]]]",
+};
+
+router.calculateRoute(routingParameters, onResult, function (error) {
+    alert(error.message);
+});
+
+let changeView = document.querySelector("#changeView");
+
+changeView.onclick = () => {
+    if (!routeResult) return;
+    // localStorage.setItem("routeOutput", JSON.stringify(routeResult));
+
+    let link = document.createElement("a");
+    link.href = "../ar.html";
+    link.click();
+};
